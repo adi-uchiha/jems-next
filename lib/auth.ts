6456cd02ffbedf06e41249e67754c5e5330608c0
adi-uchiha <adi.cs.night@gmail.com>
2025-02-12 19:41:17 +0530

    Add Github auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -78,10 +78,10 @@ export const auth = betterAuth({
 		// 	clientId: process.env.FACEBOOK_CLIENT_ID || "",
 		// 	clientSecret: process.env.FACEBOOK_CLIENT_SECRET || "",
 		// },
-		// github: {
-		// 	clientId: process.env.GITHUB_CLIENT_ID || "",
-		// 	clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
-		// },
+		github: {
+			clientId: process.env.GITHUB_CLIENT_ID || "",
+			clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
+		},
 		google: {
 			clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || "",
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",

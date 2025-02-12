d0bad511f0e90420ba0ca3f839fea99c7ae024fd
adi-uchiha <adi.cs.night@gmail.com>
2025-02-12 13:26:07 +0530

    Add more lgos

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,8 @@ import { getSessionCookie } from "better-auth";
 export async function middleware(request: NextRequest) {
 	const cookies = getSessionCookie(request);
 	if (!cookies) {
+		console.log("No session cookie found");
+		console.log(request)
 		return NextResponse.redirect(new URL("/", request.url));
 	}
 	return NextResponse.next();
